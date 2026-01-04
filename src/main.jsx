a5928997e8bc0ed8c2f5c6afcf91057a73d2da5e
Oriel Belzer <orielbelzer@gmail.com>
2026-01-04 18:01:50 +0000

    Fix build: import styles.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles.css";
 
 function showErrorOnPage(label, err) {
   const msg = err?.stack || err?.message || String(err);
